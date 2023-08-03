e016f7da3935de0cfe9c4304fd9c6c87412a295c
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-08-04 00:56:51 +0930

    chore: log on error rendering homepage

--- src/handlers/home.ts
+++ src/handlers/home.ts
@@ -2,6 +2,12 @@ import { Response } from '@nbit/bun';
 import { defineRoutes } from '../application';
 
 export default defineRoutes(app => [
-    app.get('/', () => Response.file('public/index.html')),
+    app.get('/', () => {
+        try {
+            return Response.file('public/index.html')
+        } catch (error) {
+            console.error(error);
+        }
+    }),
     app.get('/images/favicon.ico', () => Response.file('public/images/favicon.ico')),
 ]);
