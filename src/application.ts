384497e80c9b4a3bf48864dc386c59f59b1563ee
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-08-05 19:48:22 +0930

    chore: update nbit

--- src/application.ts
+++ src/application.ts
@@ -1,23 +1,9 @@
 import { join } from 'path';
-
 import { createApplication } from '@nbit/bun';
 
 const { defineRoutes, attachRoutes } = createApplication({
     root: join(import.meta.dir, '..'),
     allowStaticFrom: ['public'],
-    getContext: (request) => ({
-        auth: async () => {
-            const authHeader = request.headers.get('Authorization') ?? '';
-            const token = authHeader.startsWith('Bearer')
-                ? authHeader.split(' ')[1]
-                : undefined;
-            return isValidAuthToken(token);
-        },
-    }),
 });
-
-function isValidAuthToken(token: string | undefined) {
-    return token === 'some-valid-token';
-}
 
 export { defineRoutes, attachRoutes };
